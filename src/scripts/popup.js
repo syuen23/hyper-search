348d548e9bad8decf3ef8999b615c69768000e58
Simon Yuen <54995458+syuen23@users.noreply.github.com>
2024-07-23 00:13:33 -0400

    Add event listeners for search button and input

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,7 +1,20 @@
+import { initiateSearchQuery } from "./content/search";
+
 /**
  *  Add event listeners on DOM load
  */
 document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementById("search-btn");
     const searchInput = document.getElementById("search-input");
+
+    if (searchButton) {
+        searchButton.addEventListener("click", initiateSearchQuery);
+    }
+    if (searchInput) {
+        searchInput.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+                search();
+            }
+        });
+    }
 });
